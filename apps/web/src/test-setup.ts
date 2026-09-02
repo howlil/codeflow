42e44c8d0b3012f8aa03157bdc3fe8a0bb95005d
Mhd Ulil Abshar <87646428+howlil@users.noreply.github.com>
2026-09-03 04:40:49 +0700

    test(web): support Radix interactions in jsdom

--- apps/web/src/test-setup.ts
+++ apps/web/src/test-setup.ts
@@ -1,1 +1,18 @@
 import '@testing-library/jest-dom/vitest';
+
+if (globalThis.PointerEvent === undefined) {
+  globalThis.PointerEvent = MouseEvent as typeof PointerEvent;
+}
+
+if (HTMLElement.prototype.hasPointerCapture === undefined) {
+  HTMLElement.prototype.hasPointerCapture = () => false;
+}
+if (HTMLElement.prototype.setPointerCapture === undefined) {
+  HTMLElement.prototype.setPointerCapture = () => undefined;
+}
+if (HTMLElement.prototype.releasePointerCapture === undefined) {
+  HTMLElement.prototype.releasePointerCapture = () => undefined;
+}
+if (HTMLElement.prototype.scrollIntoView === undefined) {
+  HTMLElement.prototype.scrollIntoView = () => undefined;
+}
